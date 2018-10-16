4778c131a316c48452722a93d4e2b4a394358377
Sublime <other@Alberts-MBP.fios-router.home>
2018-10-15 22:49:13 -0400

    made code

---
+++ RationalNumber.java
@@ -0,0 +1,21 @@
+public class RationalNumber extends RealNumber{
+  private int numerator, denominator;
+  public RationalNumber(int n, int d){
+    super(3.0);
+    numerator = n;
+    denominator = d;
+  }
+  public int getNumerator(){
+    return 0; //rando
+  }
+  public int getDenominator(){
+    return 0; //rando
+  }
+  public String toString(){
+    return "e"; //rando
+  }
+  public String getValue(){
+    return "s";//rando
+    //return getNumerator()/getDenominator();
+  }
+}
---
+++ RealNumber.java
@@ -0,0 +1,12 @@
+public class RealNumber{
+  private double num;
+  public RealNumber(double dub){
+    num = dub;
+  }
+  public double getNumber(){
+    return 1.0;//rando
+  }
+  public String toString(){
+    return "e";//rando
+  }
+}
